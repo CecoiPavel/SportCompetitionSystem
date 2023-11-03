a36c8d0d1f5c746e2ca72490eaf60d14e09d5338
CecoiPavel <adresameabro@gmail.com>
2023-11-04 00:52:11 +0200

    Created the MemberType missing Enum.

---
+++ SportCompetitionSystem/Domain/MemberType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCompetitionSystem.Domain
+{
+    internal enum MemberType
+    {
+        None = 4,
+        Senior =1,
+        Junior,
+        Staff
+    }
+}
